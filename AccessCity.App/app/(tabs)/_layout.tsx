8433293402e2071b55d9802dd91f82fe75a2155e
Sheima Dridi <sheimadridi@Sheimas-MacBook-Air.local>
2026-03-12 21:15:33 +0000

    worked on the signup and login page, and removed the home button

--- AccessCity.App/app/(tabs)/_layout.tsx
+++ AccessCity.App/app/(tabs)/_layout.tsx
@@ -11,6 +11,7 @@ export default function TabLayout() {
 
   return (
     <Tabs
+      initialRouteName="map"
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
@@ -19,8 +20,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          href: null,
         }}
       />
       <Tabs.Screen
